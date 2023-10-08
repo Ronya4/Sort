1f24749f5dca81f081bbb7ad5623511c21705207
Ronya4 <147103248+Ronya4@users.noreply.github.com>
2023-10-08 21:35:02 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,23 +1,23 @@
-#include <iostream>		// Ввод-вывод в консоль
-#include <array>		// Массив
-#include <random>		// Случайные числа
-#include <algorithm>	// Нужен только std::find (для генерации рандомного массива)
+#include <iostream>		
+#include <array>		
+#include <random>		
+#include <algorithm>	
 
-static constexpr std::size_t n = 200;					// Размер массива
-static constexpr std::size_t method_count = 8;			// Кол-во методов
+static constexpr std::size_t n = 200;					
+static constexpr std::size_t method_count = 8;			
 
-#define ELEM_TYPE			short						// Тип элементов массива
-#define ARRAY_TYPE			std::array<ELEM_TYPE, n>	// Тип массива
-#define ARRAY_REF			ARRAY_TYPE&					// Тип ссылки на массив
-#define ARRAY_CONST_REF		const ARRAY_REF				// Тип константной ссылки на массив
+#define ELEM_TYPE			short						
+#define ARRAY_TYPE			std::array<ELEM_TYPE, n>	
+#define ARRAY_REF			ARRAY_TYPE&					
+#define ARRAY_CONST_REF		const ARRAY_REF				
 
-#define ITER_TYPE			ARRAY_TYPE::iterator		// Тип итератора массива
+#define ITER_TYPE			ARRAY_TYPE::iterator		
 
-// forward	- массив по возрастанию
-// back		- массив по убыванию
-// random	- случайно упорядоченный массив
+// forward	
+// back		
+// random	
 
-// Заполнение массивов элементами в соответствующем порядке
+
 void fill_arrays(ARRAY_REF forward, ARRAY_REF back, ARRAY_REF random) noexcept
 {
 	for (std::size_t i = 0; i < n; ++i)
@@ -30,7 +30,7 @@ void fill_arrays(ARRAY_REF forward, ARRAY_REF back, ARRAY_REF random) noexcept
 	}
 }
 
-// Печать массивов на экран
+
 void print_arrays(ARRAY_CONST_REF forward,
 	ARRAY_CONST_REF back, ARRAY_CONST_REF random) noexcept
 {
@@ -48,161 +48,159 @@ void print_arrays(ARRAY_CONST_REF forward,
 	std::cout << std::endl << std::endl;
 }
 
-// Предикат по умолчанию (определяет порядок сортировки, по умолчанию - по возрастанию)
+
 bool default_pred(ELEM_TYPE a, ELEM_TYPE b) noexcept
 {
 	return a > b;
 }
 
-// Параметры функций сортировки
 #define SORT_TYPE_OPTIONS ITER_TYPE first, ITER_TYPE last, bool(*pred)(ELEM_TYPE a, ELEM_TYPE b)
 #define SORT_OPTIONS (SORT_TYPE_OPTIONS = default_pred)
 
-static std::size_t comp_count = 0;	// Кол-во сравнений
-static std::size_t perm_count = 0;	// Кол-во перестановок
+static std::size_t comp_count = 0;	
+static std::size_t perm_count = 0;	
 
-// Простой обмен
 void simple_exchange_sort SORT_OPTIONS noexcept
 {
-	for (std::size_t i = 0; i < n; ++i)	// Повторение проходов n раз
+	for (std::size_t i = 0; i < n; ++i)	
 	{
-		for (ITER_TYPE j = first; j != last - 1; ++j) // Один проход с обменами
+		for (ITER_TYPE j = first; j != last - 1; ++j) 
 		{
 			++comp_count;
-			if (pred(*j, *(j + 1))) // Сравнение
+			if (pred(*j, *(j + 1))) 
 			{
 				++perm_count;
-				std::swap(*j, *(j + 1)); // Перестановка
+				std::swap(*j, *(j + 1)); 
 			}
 		}
 	}
 }
 
-// Простой выбор
+
 void simple_choice_sort SORT_OPTIONS noexcept
 {
-	for (std::size_t i = 0; i < n; ++i) // Повторение n раз
+	for (std::size_t i = 0; i < n; ++i) // ГЏГ®ГўГІГ®Г°ГҐГ­ГЁГҐ n Г°Г Г§
 	{
 		ITER_TYPE iter_max = first;
-		for (ITER_TYPE j = first + 1; j != last - i; ++j) // Выбор максимального
+		for (ITER_TYPE j = first + 1; j != last - i; ++j) 
 		{
 			++comp_count;
-			if (*iter_max < *j) // Сравнение
+			if (*iter_max < *j) 
 				iter_max = j;
 		}
 		++comp_count;
-		if (pred(*iter_max, *(last - i - 1))) // Сравнение
+		if (pred(*iter_max, *(last - i - 1))) 
 		{
 			++perm_count;
-			std::swap(*iter_max, *(last - i - 1)); // Перестановка (самый большой в конец)
+			std::swap(*iter_max, *(last - i - 1)); 
 		}
 	}
 }
 
-// Простая вставка
+
 void simple_insert_sort SORT_OPTIONS noexcept
 {
-	for (ITER_TYPE i = first + 1; i != last; ++i) // Цикл по вставляемым элементам
+	for (ITER_TYPE i = first + 1; i != last; ++i) 
 	{
-		for (ITER_TYPE j = i - 1; j != first; --j) // Вставка
+		for (ITER_TYPE j = i - 1; j != first; --j) 
 		{
 			++comp_count;
-			if (pred(*j, *(j + 1))) // Сравнение
+			if (pred(*j, *(j + 1)))
 			{
 				++perm_count;
-				std::swap(*j, *(j + 1)); // Перестановка
+				std::swap(*j, *(j + 1));
 			}
 			else
 				break;
 		}
 		++comp_count;
-		if (pred(*first, *(first + 1))) // Сравнение
+		if (pred(*first, *(first + 1))) 
 		{
 			++perm_count;
-			std::swap(*first, *(first + 1)); // Перестановка
+			std::swap(*first, *(first + 1)); 
 		}
 	}
 }
 
-// Шейкер
+
 void shayker_sort SORT_OPTIONS noexcept
 {
-	while (first != last) // Пока границы не сойдуться
+	while (first != last) 
 	{
-		for (ITER_TYPE i = first; i != last - 1; ++i) // Прямой проход с обменами
+		for (ITER_TYPE i = first; i != last - 1; ++i) 
 		{
 			++comp_count;
-			if (pred(*i, *(i + 1))) // Сравнение
+			if (pred(*i, *(i + 1))) 
 			{
 				++perm_count;
-				std::swap(*i, *(i + 1)); // Перестановка
+				std::swap(*i, *(i + 1));
 			}
 		}
 		last--;
-		if (first == last) // Пока границы не сойдуться
+		if (first == last) 
 			break;
-		for (ITER_TYPE i = last - 1; i != first; --i) // Обратный проход с обменами
+		for (ITER_TYPE i = last - 1; i != first; --i) 
 		{
 			++comp_count;
-			if (pred(*(i - 1), *i)) // Сравнение
+			if (pred(*(i - 1), *i)) 
 			{
 				++perm_count;
-				std::swap(*(i - 1), *i); // Перестановка
+				std::swap(*(i - 1), *i); 
 			}
 		}
 		first++;
 	}
 }
 
-// Спуск по куче (чтоб сделать корректную возрастающую кучу)
+
 void log_descent(ITER_TYPE current, ITER_TYPE first, ITER_TYPE last,
 	bool(*pred)(ELEM_TYPE a, ELEM_TYPE b)) noexcept
 {
-	auto i = current - first; // Номер верхнего элемента
+	auto i = current - first; 
 	auto dist = last - first;
 	if (i >= dist / 2)
 		return;
-	ITER_TYPE iter_max = first + 2 * i + 1; // Левый нижний элемент
+	ITER_TYPE iter_max = first + 2 * i + 1; 
 	++comp_count;
-	if (iter_max + 1 != last && pred(*(iter_max + 1), *iter_max)) // Сравнение
-		++iter_max; // Правый нижний элемент
+	if (iter_max + 1 != last && pred(*(iter_max + 1), *iter_max)) 
+		++iter_max; 
 	++comp_count;
-	if (pred(*iter_max, *current)) // Сравнение
+	if (pred(*iter_max, *current)) 
 	{
 		++perm_count;
-		std::swap(*iter_max, *current); // Перестановка
-		log_descent(iter_max, first, last, pred); // Спуск по куче
+		std::swap(*iter_max, *current);
+		log_descent(iter_max, first, last, pred); 
 	}
 }
 
-// Пирамидальная
+
 void heap_sort SORT_OPTIONS noexcept
 {
 	for (ITER_TYPE::difference_type i = n / 2 - 1; i >= 0; --i)
-		log_descent(first + i, first, last, pred); // Создание возрастающей кучи
-	for (std::size_t i = 0; i < n - 1; ++i) // Сортировка
+		log_descent(first + i, first, last, pred); 
+	for (std::size_t i = 0; i < n - 1; ++i) 
 	{
 		++perm_count;
-		std::swap(*first, *(last - 1)); // Перестановка
+		std::swap(*first, *(last - 1)); 
 		--last;
-		log_descent(first, first, last, pred); // Возврат кучи в корректное состояние
+		log_descent(first, first, last, pred); 
 	}
 }
 
-// Шелла
+
 void shell_sort SORT_OPTIONS noexcept
 {
-	for (std::size_t i = log2(n); i > 0; i = log2(i)) // Шаг цепочки
+	for (std::size_t i = log2(n); i > 0; i = log2(i)) 
 	{
-		for (ITER_TYPE j = first + i;; j += i) // Далее простая вставка
+		for (ITER_TYPE j = first + i;; j += i) 
 		{
 			for (ITER_TYPE k = j - i;; k -= i)
 			{
 				++comp_count;
-				if (pred(*k, *(k + i))) // Сравнение
+				if (pred(*k, *(k + i))) 
 				{
 					++perm_count;
-					std::swap(*k, *(k + i)); // Перестановка
+					std::swap(*k, *(k + i)); 
 				}
 				else
 					break;
@@ -215,64 +213,64 @@ void shell_sort SORT_OPTIONS noexcept
 	}
 }
 
-// Бинарная вставка
+
 void binary_insert_sort SORT_OPTIONS noexcept
 {
-	for (ITER_TYPE i = first + 1; i != last; ++i) // Цикл по вставляемым элементам
+	for (ITER_TYPE i = first + 1; i != last; ++i) 
 	{
-		ITER_TYPE l = first; // Левая граница
-		ITER_TYPE r = i; // Правая граница
-		while (r != l) // Пока границы не сойдуться
+		ITER_TYPE l = first; 
+		ITER_TYPE r = i; 
+		while (r != l) 
 		{
 			long long dist = r - l;
-			ITER_TYPE half = l + dist / 2; // Деление отсортированной части пополам
+			ITER_TYPE half = l + dist / 2;
 			++comp_count;
-			if (pred(*half, *i)) // Сравнение
+			if (pred(*half, *i))
 				r = half;
 			else
 				l = half + 1;
 		}
-		for (ITER_TYPE j = i; j != l; --j) // Вставка
+		for (ITER_TYPE j = i; j != l; --j)
 		{
 			++perm_count;
-			std::swap(*(j - 1), *j); // Перестановка
+			std::swap(*(j - 1), *j); 
 		}
 	}
 }
 
-// Быстрая
+
 void quick_sort SORT_OPTIONS noexcept
 {
-	if (last - first <= 1) // Если размер сортируемой части <= 1 - массив отсортирован
+	if (last - first <= 1) 
 		return;
 	++comp_count;
-	if (*(last - 1) < *first) // Сравнение
+	if (*(last - 1) < *first)
 	{
 		++perm_count;
-		std::swap(*(last - 1), *first); // Перестановка
+		std::swap(*(last - 1), *first); 
 	}
-	short pivot = *(last - 1); // Опорный элемент
-	ITER_TYPE wall = first; // Граница деления массивов
-	bool state = false; // Состояние алгоритма (false - элемент на границе меньше опорного, true - иначе)
-	for (ITER_TYPE i = first + 1; i != last - 1; ++i) // Цикл по элементам сортируемой части
+	short pivot = *(last - 1); 
+	ITER_TYPE wall = first; 
+	bool state = false; 
+	for (ITER_TYPE i = first + 1; i != last - 1; ++i)
 	{
 		++comp_count;
-		if (state && pred(pivot, *i)) // Сравнение
+		if (state && pred(pivot, *i)) 
 		{
 			++perm_count;
-			std::swap(*i, *(wall + 1)); // Перестновка
+			std::swap(*i, *(wall + 1)); 
 			++wall;
 		}
-		else if (!state && pred(pivot, *i)) // Сравнение
+		else if (!state && pred(pivot, *i)) 
 			++wall;
 		else
 			state = true;
 	}
-	quick_sort(first, wall + 1, pred); // Разделение и рекурсивный вызов
+	quick_sort(first, wall + 1, pred); 
 	quick_sort(wall + 1, last, pred);
 }
 
-// Типы сортировок
+
 enum sort_type
 {
 	SIMPLE_EXCHANGE = 0,
@@ -285,9 +283,9 @@ enum sort_type
 	QUICK = 7
 };
 
-typedef void(*sort_ptr)(SORT_TYPE_OPTIONS); // Тип сортирующей функции
+typedef void(*sort_ptr)(SORT_TYPE_OPTIONS); 
 
-// Шаблонный вызов сортирующей функции
+
 template <sort_type type> void sort SORT_OPTIONS noexcept
 {
 	static sort_ptr sort_arr[method_count] = {
@@ -299,10 +297,10 @@ template <sort_type type> void sort SORT_OPTIONS noexcept
 
 void main()
 {
-	ARRAY_TYPE forward, back, random;		// Массивы
-	std::size_t table[method_count][3][2];	// Таблицы с результатми
+	ARRAY_TYPE forward, back, random;		
+	std::size_t table[method_count][3][2];	
 
-	// Прогон простого обмена
+	
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -321,7 +319,7 @@ void main()
 	std::cout << "AFTER (SIMPLE EXCHANGE):" << std::endl;
 	print_arrays(forward, back, random);
 
-	// Прогон простого выбора
+	
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -340,7 +338,6 @@ void main()
 	std::cout << "AFTER (SIMPLE CHOICE):" << std::endl;
 	print_arrays(forward, back, random);
 
-	// Прогон простой вставки
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -359,7 +356,6 @@ void main()
 	std::cout << "AFTER (SIMPLE INSERT):" << std::endl;
 	print_arrays(forward, back, random);
 
-	// Прогон шейкера
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -378,7 +374,6 @@ void main()
 	std::cout << "AFTER (SHAYKER):" << std::endl;
 	print_arrays(forward, back, random);
 
-	// Прогон пирамидальной
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -397,7 +392,6 @@ void main()
 	std::cout << "AFTER (HEAP):" << std::endl;
 	print_arrays(forward, back, random);
 
-	// Прогон шелла
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -416,7 +410,6 @@ void main()
 	std::cout << "AFTER (SHELL):" << std::endl;
 	print_arrays(forward, back, random);
 
-	// Прогон бинарной вставки
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -435,7 +428,6 @@ void main()
 	std::cout << "AFTER (BINARY INSERT):" << std::endl;
 	print_arrays(forward, back, random);
 
-	// Прогон быстрой
 	fill_arrays(forward, back, random);
 	std::cout << "BEFOR:" << std::endl;
 	print_arrays(forward, back, random);
@@ -453,8 +445,6 @@ void main()
 	table[7][2][1] = perm_count;
 	std::cout << "AFTER (QUICK):" << std::endl;
 	print_arrays(forward, back, random);
-
-	// Далее печать таблицы с результатами
 
 	const char* methods[method_count] = {
 		"SIMPLE EXCHANGE\t", "SIMPLE CHOICE\t", "SIMPLE INSERT\t",
